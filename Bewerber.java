48a301698b7bc44f51e35634d56dcb63c55f0540
LinnartKr <133028633+LinnartKr@users.noreply.github.com>
2023-05-22 15:16:53 +0200

    Update Bewerber.java

--- Bewerber.java
+++ Bewerber.java
@@ -1,1 +1,30 @@
 
+public class Bewerbung {
+    private int anzahlPersonen;
+    private int haustiere;
+    private boolean raucher;
+    private boolean vorstrafen;
+
+    public Bewerbung(int anzahlPersonen, int haustiere, boolean raucher, boolean vorstrafen) {
+        this.anzahlPersonen = anzahlPersonen;
+        this.haustiere = haustiere;
+        this.raucher = raucher;
+        this.vorstrafen = vorstrafen;
+    }
+
+    public int getAnzahlPersonen() {
+        return anzahlPersonen;
+    }
+
+    public int getHaustiere() {
+        return haustiere;
+    }
+
+    public boolean isRaucher() {
+        return raucher;
+    }
+
+    public boolean hasVorstrafen() {
+        return vorstrafen;
+    }
+}
