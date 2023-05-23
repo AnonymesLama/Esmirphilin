c46d21febd58a8245818b39b2203d9e81f600ed4
esmaersan <134282989+esmaersan@users.noreply.github.com>
2023-05-23 19:35:40 +0200

    JavaDoc Kommentare hinzugefügt

--- Konto.java
+++ Konto.java
@@ -1,35 +1,72 @@
 
+/**
+ * Klasse Konto
+ * Zweck: Abstrakte Klasse, die einen Nutzernamen, ein Passwort und eine E-Mail-Adresse hat.
+ * 
+ * @author
+ * @version 1
+ * Historie:
+ */
+
 public abstract class Konto {
-	// Klasse Konto: Abstrakte Klasse, die einen Nutzernamen, ein Passwort und eine E-Mail-Adresse hat.
+	
     private String nutzername;
     private String passwort;
     private String email;
 
-    // Konstruktor
+    /**
+     * Konto Konstruktor
+     * Vorbedingung: Es darf vorausgesetzt werden, dass alle nötigen Attribute deklariert sind.
+     * Effekt: Instanziierung eines neuen Kontos (Users).
+     * 
+     * @param nutzername
+     * @param passwort  
+     * @param email     
+     */
+
     public Konto(String nutzername, String passwort, String email) {
         this.nutzername = nutzername;
         this.passwort = passwort;
         this.email = email;
     }
 
-    // Getter-Methoden
+    /**
+     * Alle folgenden Methoden sind Getter-Methoden
+     *
+     * @return geben jeweils den Wert der Variable zurück
+     */
+    
     public String getNutzername() {
         return nutzername;
     }
-
+    
+    
     public String getPasswort() {
         return passwort;
     }
 
+    
     public String getEmail() {
         return email;
     }
 
-    // Setter-Methoden
+    /**
+     * Die folgende Methode ist eine Setter-Methode
+     *
+     * @return weißen den Wert des Parameters einer Variable zu
+     */
+    
     public void setPasswort(String passwort) {
         this.passwort = passwort;
     }
 
+    /**
+     * ToString Fachmethode
+     * Effekt: gibt eine String-Repräsentation der Klasse zurück
+     *
+     * @return String (siehe Effekt)
+     */
+    
     @Override
     public String toString() {
         return "Konto{" +
