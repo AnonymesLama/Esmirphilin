d3079ad9fc03c1171e5d730591e09525eb41e04d
esmaersan <134282989+esmaersan@users.noreply.github.com>
2023-05-23 19:32:13 +0200

    JavaDoc Kommentare hinzugefügt

--- Nutzer.java
+++ Nutzer.java
@@ -1,4 +1,13 @@
-// Klasse Nutzer: Abstrakte Klasse, die von Konto erbt und zusätzlich die Attribute Vorname, Nachname, Anrede, Telefonnummer und Adresse hat.
+
+/**
+ * Klasse Nutzer
+ * Zweck: Abstrakte Klasse, die von Konto erbt
+          und zusätzlich die Attribute Vorname, Nachname, Anrede, Telefonnummer und Adresse hat.
+ * @author
+ * @version 1
+ * Historie:
+ */
+
 abstract class Nutzer extends Konto {
     private String vorname;
     private String nachname;
@@ -6,7 +15,21 @@ abstract class Nutzer extends Konto {
     private String telefonnummer;
     private String adresse;
 
-    // Konstruktor
+    /**
+     * Nutzer Konstruktor
+     * Vorbedingung: Es darf vorausgesetzt werden, dass alle nötigen Attribute deklariert sind.
+     * Effekt: Instanziierung eines neuen Nutzers.
+     *
+     * @param nutzername    the nutzername
+     * @param passwort      the passwort
+     * @param email         the email
+     * @param vorname       the vorname
+     * @param nachname      the nachname
+     * @param anrede        the anrede
+     * @param telefonnummer the telefonnummer
+     * @param adresse       the adresse
+     */
+
     public Nutzer(String nutzername, String passwort, String email, String vorname, String nachname, String anrede, String telefonnummer, String adresse) {
         super(nutzername, passwort, email);
         this.vorname = vorname;
@@ -16,31 +39,47 @@ abstract class Nutzer extends Konto {
         this.adresse = adresse;
     }
 
-    // Getter-Methoden
+
+    /**
+     * Alle folgenden Methoden sind Getter-Methoden
+     *
+     * @return geben jeweils den Wert der Variable zurück
+     */
+
     public String getVorname() {
         return vorname;
     }
+
 
     public String getNachname() {
         return nachname;
     }
 
+
     public String getAnrede() {
         return anrede;
     }
+
 
     public String getTelefonnummer() {
         return telefonnummer;
     }
 
+
     public String getAdresse() {
         return adresse;
     }
 
-    // Setter-Methoden
+    /**
+     * Alle folgenden Methoden sind Setter-Methoden
+     *
+     * @return weißen den Wert des Parameters einer Variable zu
+     */
+
     public void setVorname(String vorname) {
         this.vorname = vorname;
     }
+
 
     public void setNachname(String nachname) {
         this.nachname = nachname;
@@ -50,13 +89,22 @@ abstract class Nutzer extends Konto {
         this.anrede = anrede;
     }
 
+
     public void setTelefonnummer(String telefonnummer) {
         this.telefonnummer = telefonnummer;
     }
 
+
     public void setAdresse(String adresse) {
         this.adresse = adresse;
     }
+
+    /**
+     * ToString Fachmethode
+     * Effekt: gibt eine String-Repräsentation der Klasse zurück
+     *
+     * @return String (siehe Effekt)
+     */
 
     @Override
     public String toString() {
